a63d469900f28189d99fb84096d2f23fdc6e6767
20026866 <20026866@DESKTOP-PQVPABL.mshome.net>
2021-07-21 14:44:43 +0800

    line 162 in code

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -159,6 +159,7 @@ public class ResourceCentre {
 		String output = "";
 		// write your code here
 		// Ammar is doing this part
+		// ammar still doing this part
 		
 		for(int i = 0;  i < chromebookList.size(); i++) {
 			output += String.format("%-10s %-30s %-10s, %-10s %-20s", chromebookList.get(i).getAssetTag(), chromebookList.get(i).getDescription(), ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()), chromebookList.get(i).getDueDate(), chromebookList.get(i).getOs());
