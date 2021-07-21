489a2c0e6dedb04336f82b63007f752adf3481ba
20026866 <20026866@DESKTOP-PQVPABL.mshome.net>
2021-07-21 14:48:12 +0800

    line 162 of code, push tester

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -159,7 +159,7 @@ public class ResourceCentre {
 		String output = "";
 		// write your code here
 		// Ammar is doing this part
-		// ammar still doing this part e
+		// ammar still doing this part ehh
 		
 		for(int i = 0;  i < chromebookList.size(); i++) {
 			output += String.format("%-10s %-30s %-10s, %-10s %-20s", chromebookList.get(i).getAssetTag(), chromebookList.get(i).getDescription(), ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()), chromebookList.get(i).getDueDate(), chromebookList.get(i).getOs());
