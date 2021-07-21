900ec815955835e45e5785d3719fd6bca16e0091
20019666 <20019666@myrp.edu.sg>
2021-07-21 14:32:35 +0800

    Ammar has done code for retrieveAllhromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -160,8 +160,12 @@ public class ResourceCentre {
 		// write your code here
 		// Ammar is doing this part
 		
-		
+		for(int i = 0;  i < chromebookList.size(); i++) {
+			output += String.format("%-10s %-30s %-10s, %-10s %-20s", chromebookList.get(i).getAssetTag(), chromebookList.get(i).getDescription(), ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()), chromebookList.get(i).getDueDate(), chromebookList.get(i).getOs());
+		}
 		return output;
+		
+		//retrieveAllChromebook is done
 	}
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
 		
